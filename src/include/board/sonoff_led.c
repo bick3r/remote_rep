29609a6d1ec1daf9fca19c50cb8c6f04154f32ce
Wojciech Bielawskiw <64120390+bick3r@users.noreply.github.com>
2020-09-10 13:18:41 +0200

    Update sonoff_led.c

--- src/include/board/sonoff_led.c
+++ src/include/board/sonoff_led.c
@@ -23,7 +23,7 @@
 
 void ICACHE_FLASH_ATTR supla_esp_board_set_device_name(char *buffer, uint8 buffer_size) {
 	
-		ets_snprintf(buffer, buffer_size, "Sonoff led timer");
+		ets_snprintf(buffer, buffer_size, "Sonoff led timer_1");
 }
 char *ICACHE_FLASH_ATTR supla_esp_board_cfg_html_template(
     char dev_name[25], const char mac[6], const char data_saved) {
