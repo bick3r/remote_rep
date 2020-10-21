b439e80452038ddf1bf79f652dbad3d00a0be551
Wojciech Bielawskiw <64120390+bick3r@users.noreply.github.com>
2020-10-21 10:29:59 +0200

    Update sonoff_led_ds.c

--- src/include/board/sonoff_led_ds.c
+++ src/include/board/sonoff_led_ds.c
@@ -149,9 +149,9 @@ char *ICACHE_FLASH_ATTR supla_esp_board_cfg_html_template(
       (unsigned char)mac[4], (unsigned char)mac[5], supla_esp_cfg.WIFI_SSID,
       supla_esp_cfg.Server, supla_esp_cfg.Email,
       supla_esp_cfg.StatusLedOff == 0 ? "selected" : "",
-      supla_esp_cfg.StatusLedOff == 1 ? "selected" : "",
-      supla_esp_cfg.FirmwareUpdate == 0 ? "selected" : "",
-      supla_esp_cfg.FirmwareUpdate == 1 ? "selected" : "");
+      supla_esp_cfg.StatusLedOff == 1 ? "selected" : "");
+      //supla_esp_cfg.FirmwareUpdate == 0 ? "selected" : "",
+      //supla_esp_cfg.FirmwareUpdate == 1 ? "selected" : "");
 
   return buffer;
 }
