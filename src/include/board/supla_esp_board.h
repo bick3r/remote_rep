f27653178af8b5e9990d6fb7b48a2ff977aeb527
bick3r <wjcchblwsk@gmail.com>
2020-10-21 18:32:02 +0200

    xx

--- src/include/board/supla_esp_board.h
+++ src/include/board/supla_esp_board.h
@@ -93,6 +93,9 @@
 #elif defined(__BOARD_sonoff_led)
 #include "board/sonoff_led.h"
 
+#elif defined(__BOARD_sonoff_led_14)
+#include "board/sonoff_led_14.h"
+
 #elif defined(__BOARD_sonoff_led_ds)
 #include "board/sonoff_led_ds.h"
 
